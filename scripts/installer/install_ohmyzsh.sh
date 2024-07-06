fdff005523ddc3a31b95587fa938033a48f4f717
Unmedius <unmedius@gmail.com>
2024-07-06 15:43:42 +0300

    pc separation v6

--- scripts/installer/install_ohmyzsh.sh
+++ scripts/installer/install_ohmyzsh.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
 
-# Install Oh My Zsh
+# Check if Oh My Zsh is already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
+    # Install Oh My Zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
     echo "Oh My Zsh is already installed."
+fi
+
+# Clone Powerlevel10k theme if it's not already present
+POWERLEVEL10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+if [ ! -d "$POWERLEVEL10K_DIR" ]; then
+    echo "Cloning Powerlevel10k theme..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$POWERLEVEL10K_DIR"
+else
+    echo "Powerlevel10k theme is already installed."
 fi
